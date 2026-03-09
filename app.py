0cf9145e6b0807dc8c5b7b379a1feb49cf9fd7de
kwabsntim <daffourkwabena@gmail.com>
2026-03-09 14:49:49 +0000

    add QR code generation and update index template to display it

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import socket 
-
+import qrcode
+import io
+import base64
 
 
 app = Flask(__name__)
@@ -25,13 +27,41 @@ def get_local_ip():
         return "could not determine a non-loopback IP address"
     return local_ip
 
+#Qr code  function 
+def generate_qr_code(data):
+    ''''
+    Generate a QR code from the provided data and return it as a base64-encoded string.
+    '''
 
+    qr=qrcode.QRCode(version=1,box_size=10,border=4,error_correction=qrcode.constants.ERROR_CORRECT_L)
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    #making the qr code image 
+    img=qr.make_image(fill_color="black",back_color="white")
+
+    #save the image to the buffer
+
+    buffer=io.BytesIO()
+    img.save(buffer,format="PNG")
+    buffer.seek(0)
+
+    #encode the image to base64
+    img_bytes=buffer.read()
+    base64_bytes=base64.b64encode(img_bytes)
+
+    base64_string=base64_bytes.decode('utf-8')
+    return base64_string
+
+
+data=get_local_ip()
+qr_base64=generate_qr_code(data)
 
 
 #routes for the application
 @app.route("/")
 def hello_world():
-    return render_template('index.html',qr_code_data=get_local_ip())
+    return render_template('index.html',qr_code_img=qr_base64)
 
 
 
